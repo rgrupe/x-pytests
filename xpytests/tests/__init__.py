c6c9f0497f70d5bd69a95de5111cff38e7c07f05
Robert Grupe <robert@rgrupe.com>
2019-09-21 14:51:07 -0500

    Initial test examples.

---
+++ xpytests/tests/__init__.py
@@ -0,0 +1,2 @@
+# This needs to be determined how can be used - need at least for tests??
+# https://timothybramlett.com/How_to_create_a_Python_Package_with___init__py.html
---
+++ xpytests/tests/test_app.py
@@ -0,0 +1,4 @@
+from xpytests import app
+
+def test_answer():
+    assert app.func(3) == 5
